5dd8e7be97f9ef69937230c272727d365519d26e
bhavanukuntla <myselfbhavani@yahoo.co.in>
2024-03-22 06:16:47 +0530

    project setup

---
+++ src/main/java/com/tax/management/employee/tax/management/App.java
@@ -0,0 +1,13 @@
+package com.tax.management.employee.tax.management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
